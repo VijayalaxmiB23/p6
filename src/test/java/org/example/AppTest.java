841f03c5b7ae5d50138ea5a1f1977158d8a4ac26
VijayalaxmiB23 <bvijayalaxmi232004@gmail.com>
2025-05-18 14:12:36 +0530

    initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+// this is just a comment to check verify
+
+public class AppTest {
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://msindhu46.github.io/web/");// here enter static website URL from your GITHUB
+    }
+
+    @Test
+    public void titleValidationTest(){
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "CSDS";
+        Assert.assertEquals(actualTitle, expectedTitle);
+        assertTrue(true, "Title should contain 'CSDS'");
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.quit();
+    }
+
+}
